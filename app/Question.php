70d92e14ea74043eec99bd669c9e0942671c9d0c
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-16 23:12:24 +0530

    Added question database seeder

--- app/Question.php
+++ app/Question.php
@@ -13,4 +13,11 @@ class Question extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
