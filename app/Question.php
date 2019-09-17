cef0797de5991ab3b1944ffc3ced61adc47e1597
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-17 12:28:42 +0530

    Added css styling for votes, answers and views

--- app/Question.php
+++ app/Question.php
@@ -30,4 +30,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return 'answered-accept';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
+    }
 }
