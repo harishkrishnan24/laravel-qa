fa75b4246d976dfb0e52f24680fd40d326f7a4b1
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-24 02:57:12 +0530

    Display answers for questions

--- app/Answer.php
+++ app/Answer.php
@@ -30,4 +30,9 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
     }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
