044d5fef4d2399b6ff5859af3a3f0d29386a9c53
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-28 00:51:21 +0530

    Add vote controls to question and answer

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,16 +5,32 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+                <div class="card-body">
+                <div class="card-title">
                     <div class="d-flex align-items-center">
                     <h1>{{ $question->title }}</h1>
                     <div class="ml-auto">
                         <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                     </div>
-                    </div></div>
-
-                <div class="card-body">
-                    {!! $question->body_html !!}
+                    </div>
+                </div>
+                <hr>
+                <div class="media">
+                    <div class="d-flex flex-column vote-controls">
+                        <a title="This question is useful" class="vote-up">
+                            <i class="fas fa-caret-up fa-3x"></i>
+                        </a>
+                        <span class="votes-count">1230</span>
+                        <a title="This question is not useful" class="vote-down off">
+                            <i class="fas fa-caret-down fa-3x"></i>
+                        </a>
+                        <a title="Click to mark as favorite question (Click again to undo" class="favorite mt-2 favorited">
+                            <i class="fas fa-star fa-2x"></i>
+                            <span class="favorites-count">123</span>
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        {!! $question->body_html !!}
                      <div class="float-right">
                                     <span class="text-muted">Asked {{ $question->created_date }}</span>
                                     <div class="media mt-2">
@@ -27,6 +43,8 @@
                                         </div>
                                     </div>
                                 </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
@@ -41,6 +59,18 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                        <a title="This answer is useful" class="vote-up">
+                            <i class="fas fa-caret-up fa-3x"></i>
+                        </a>
+                        <span class="votes-count">1230</span>
+                        <a title="This answer is not useful" class="vote-down off">
+                            <i class="fas fa-caret-down fa-3x"></i>
+                        </a>
+                        <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                            <i class="fas fa-check fa-2x"></i>
+                        </a>
+                    </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
