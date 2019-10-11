7ec8826afce1a137be3a1e1a974ec4513c181744
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-10-11 23:50:31 +0530

    Add votes controller to answers

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -10,6 +10,6 @@ $factory->define(Answer::class, function (Faker $faker) {
     return [
         'body' => $faker->paragraphs(rand(3, 7), true),
         'user_id' => App\User::pluck('id')->random(),
-        'votes_count' => rand(0, 5)
+        // 'votes_count' => rand(0, 5)
     ];
 });
